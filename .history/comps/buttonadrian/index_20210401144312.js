3f81b63e73e1c313687d6936c8527c5b0453a8d3
AdrianTejada <atejada@my.bcit.ca>
2021-04-01 14:44:52 -0700

    second commit

---
+++ .history/comps/buttonadrian/index_20210401144312.js
@@ -0,0 +1,22 @@
+function ButtonAdrianUI({ 
+  
+    width="200px", height="200px",borderRadius="20px",color="yellow",
+  
+  }){
+    return (
+      <div>
+        <input type="button"  style=
+        {{width:width,
+          height:height,
+          borderRadius:borderRadius,
+          color:color
+        }}> 
+        
+        </input>
+        <span>hello this is adrians component</span>
+        <h3>Adrian's component</h3> 
+      </div>
+    )
+  }
+
+  export default ButtonAdrianUI;
